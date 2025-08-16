a8ea444036ddbfa2db7e6d83a02d5d0a537c75a1
DontKnow <seinao2768@gmail.com>
2025-08-15 21:56:03 -0300

    atividade 12

---
+++ atividade12.py
@@ -0,0 +1,9 @@
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+
+def somalist(lista1, lista2):
+    somalista = lista1 + lista2
+    print(somalista)
+
+somalist(natureza, tecnologia)
